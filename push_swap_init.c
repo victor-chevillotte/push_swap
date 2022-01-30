781d3898bd1300a04d0981f1a0372f912c9fe92d
Victor Chevillotte <46164574+victor-chevillotte@users.noreply.github.com>
2022-01-30 16:22:09 +0100

    norme init ok

--- push_swap_init.c
+++ push_swap_init.c
@@ -6,7 +6,7 @@
 /*   By: vchevill <vchevill@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 13:03:02 by vchevill          #+#    #+#             */
-/*   Updated: 2022/01/30 16:08:18 by vchevill         ###   ########.fr       */
+/*   Updated: 2022/01/30 16:21:06 by vchevill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,10 @@ int	ft_findnum(char **num_tab, char *num)
 {
 	int	i;
 	int	numberofnum;
-	int numi;
-	
+	int	numi;
+
 	numi = ft_atoi(num);
 	i = 0;
-	
 	numberofnum = 0;
 	while (num_tab[i])
 	{
@@ -34,7 +33,7 @@ int	ft_findnum(char **num_tab, char *num)
 int	ft_isnum(char *num)
 {
 	int	i;
-	
+
 	i = 0;
 	while (num[i])
 	{
@@ -51,11 +50,11 @@ int	ft_checkargs(char **num_tab)
 	char	*itoa;
 
 	i = 0;
-
 	while (num_tab[i])
 	{
 		itoa = ft_itoa(ft_atoi(num_tab[i]));
-		if ((ft_strncmp(num_tab[i], itoa, ft_strlen(itoa)) != 0 && ft_atoi(num_tab[i]) != 0)
+		if ((ft_strncmp(num_tab[i], itoa, ft_strlen(itoa)) != 0
+				&& ft_atoi(num_tab[i]) != 0)
 			|| ft_isnum(num_tab[i]) != 0 || ft_findnum(num_tab, num_tab[i]) > 1)
 		{
 			free(itoa);
@@ -68,85 +67,23 @@ int	ft_checkargs(char **num_tab)
 	return (0);
 }
 
-int ft_sizeof(char **num_tab)
+int	ft_sizeof(char **num_tab)
 {
 	int	i;
-	
+
 	i = 0;
 	while (num_tab[i])
 	{
 		i++;
 	}
-		
 	return (i);
 }
 
-int	ft_init_pushswap(int argc, char **argv, t_pile *pila, t_pile *pilb)
+void	ft_match_index(int argc, int *tmp, int *tmpnotsorted, t_pile *pila)
 {
-	int		i;
-	char	**tab;
-	int		*tmp;
-	int		*tmpnotsorted;
-	int		j;
+	int	i;
+	int	j;
 
-	i = 0;
-	j = 0;
-	if (argc == 1)
-	{
-		tab = ft_split(argv[1], ' ');
-		if (!tab)
-			return (1);
-		argc = ft_sizeof(tab);
-	}
-	else 
-	{
-		tab = malloc(sizeof(char *) * (argc + 1));
-		if (!tab)
-			return (1);
-		while (argv[++i])
-			tab[i - 1] = ft_strdup(argv[i]);
-		tab[i - 1] = NULL;
-	}
-	if (ft_checkargs(tab) == 1)
-		return (1);
-	tmpnotsorted = malloc(sizeof(int) * (argc));
-	if (!tmpnotsorted)
-		return (1);
-	tmp = malloc(sizeof(int) * (argc));
-	if (!tmp)
-		return (1);
-	pila->piletop = argc - 1;
-	pila->letter = 'A';
-	pila->list = malloc(sizeof(int) * (pila->piletop + 1));
-	if (!(pila->list))
-		return (1);
-	pilb->piletop = -1;
-	pilb->letter = 'B';
-	pilb->list = malloc(sizeof(int) * (pila->piletop + 1));
-	if (!(pilb->list))
-		return (1);
-	i = argc - 1;
-	while (i >= 0)
-	{
-		tmp[argc - i - 1] = ft_atoi(tab[i]);
-		tmpnotsorted[argc - i - 1] = ft_atoi(tab[i]);
-		i--;
-	}
-	/*i = pila->piletop + 1;
-	while (--i >= 0) *int non triee
-	{
-		ft_putnbr_fd(tmpnotsorted[i], 1);
-		ft_putchar_fd('\n', 1);
-	}*/
-		quick_sort(tmp, argc);
-
-	/*i = 0;
-	while (i < pila->piletop) *int triee
-	{
-		ft_putnbr_fd(tmp[i], 1);
-		ft_putchar_fd('\n', 1);
-		i++;
-	}*/
 	j = 0;
 	i = 0;
 	while (i < argc)
@@ -160,11 +97,80 @@ int	ft_init_pushswap(int argc, char **argv, t_pile *pila, t_pile *pilb)
 		j = 0;
 		i++;
 	}	
+	free(tmpnotsorted);
+	free(tmp);
+}
+
+int	ft_init_3(int argc, char **tab, t_pile *pila, t_pile *pilb)
+{
+	int		*tmp;
+	int		*tmpnotsorted;
+	int		i;
+
+	i = argc -1;
+	tmpnotsorted = malloc(sizeof(int) * (argc));
+	if (!tmpnotsorted)
+		return (1);
+	tmp = malloc(sizeof(int) * (argc));
+	if (!tmp)
+		return (1);
+	while (i >= 0)
+	{
+		tmp[argc - i - 1] = ft_atoi(tab[i]);
+		tmpnotsorted[argc - i - 1] = ft_atoi(tab[i]);
+		i--;
+	}
+	quick_sort(tmp, argc);
+	ft_match_index(argc, tmp, tmpnotsorted, pila);
 	i = -1;
 	while (tab[++i])
 		free(tab[i]);
 	free (tab);
-	free(tmpnotsorted);
-	free(tmp);
+	return (0);
+}
+
+int	ft_init_2(int argc, char **tab, t_pile *pila, t_pile *pilb)
+{
+	pila->piletop = argc - 1;
+	pila->letter = 'A';
+	pila->list = malloc(sizeof(int) * (pila->piletop + 1));
+	if (!(pila->list))
+		return (1);
+	pilb->piletop = -1;
+	pilb->letter = 'B';
+	pilb->list = malloc(sizeof(int) * (pila->piletop + 1));
+	if (!(pilb->list))
+		return (1);
+	if (ft_init_3(argc, tab, pila, pilb) == 1)
+		return (1);
+	return (0);
+}
+
+int	ft_init_pushswap(int argc, char **argv, t_pile *pila, t_pile *pilb)
+{
+	int		i;
+	char	**tab;
+	int		j;
+
+	i = 0;
+	j = 0;
+	if (argc == 1)
+	{
+		tab = ft_split(argv[1], ' ');
+		if (!tab)
+			return (1);
+		argc = ft_sizeof(tab);
+	}
+	else
+	{
+		tab = malloc(sizeof(char *) * (argc + 1));
+		if (!tab)
+			return (1);
+		while (argv[++i])
+			tab[i - 1] = ft_strdup(argv[i]);
+		tab[i - 1] = NULL;
+	}
+	if (ft_checkargs(tab) == 1 || ft_init_2(argc, tab, pila, pilb) == 1)
+		return (1);
 	return (0);
 }
