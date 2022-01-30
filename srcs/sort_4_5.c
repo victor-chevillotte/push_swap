9a57da71fc57f432cfb3c310e6dc82c1ef236788
Victor Chevillotte <46164574+victor-chevillotte@users.noreply.github.com>
2022-01-30 20:24:59 +0100

    fixed 4

--- srcs/sort_4_5.c
+++ srcs/sort_4_5.c
@@ -6,7 +6,7 @@
 /*   By: vchevill <vchevill@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 21:06:41 by vchevill          #+#    #+#             */
-/*   Updated: 2022/01/30 20:02:02 by vchevill         ###   ########.fr       */
+/*   Updated: 2022/01/30 20:24:12 by vchevill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,13 +51,15 @@ void static	ft_sort_4_5_part3(t_pile *pila, t_pile *pilb)
 
 void static	ft_sort_4_5_part2(t_pile *pila, t_pile *pilb)
 {
-	if (pila->list[pila->piletop] == pilb->list[pilb->piletop] + 1)
+	if (pila->piletop >= 0
+		&& pila->list[pila->piletop] == pilb->list[pilb->piletop] + 1)
 	{
 		ft_push_pile(pila, pilb, a);
 		ft_rotate_reverse_pile(pila, pilb, a);
 		ft_rotate_reverse_pile(pila, pilb, a);
 	}
-	else if (pila->list[pila->piletop] == pilb->list[pilb->piletop] - 1)
+	else if (pila->piletop >= 0
+		&& pila->list[pila->piletop] == pilb->list[pilb->piletop] - 1)
 	{
 		ft_rotate_pile(pila, pilb, a);
 		ft_push_pile(pila, pilb, a);
@@ -73,7 +75,8 @@ void static	ft_sort_4_5_part2(t_pile *pila, t_pile *pilb)
 
 void	ft_sort_4_5(t_pile *pila, t_pile *pilb)
 {
-	ft_push_pile(pila, pilb, b);
+	if (pila->piletop == 4)
+		ft_push_pile(pila, pilb, b);
 	ft_push_pile(pila, pilb, b);
 	ft_sort_0_3(pila, pilb);
 	if (pila->list[0] < pilb->list[pilb->piletop])
