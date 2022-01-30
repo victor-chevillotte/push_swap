901e911049107dfea40ea5381ed3a9323c23747c
Victor Chevillotte <46164574+victor-chevillotte@users.noreply.github.com>
2022-01-30 20:07:35 +0100

    fxed static

--- srcs/main.c
+++ srcs/main.c
@@ -1,54 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.c                                        :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vchevill <vchevill@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 17:06:39 by vchevill          #+#    #+#             */
-/*   Updated: 2022/01/30 19:59:05 by vchevill         ###   ########.fr       */
+/*   Updated: 2022/01/30 20:07:29 by vchevill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-void	ft_sort_6_100(t_pile *pila, t_pile	*pilb)
-{
-	//int	hold_first;
-	//int	hold_second;
-	int	i;
-
-	i = pila->piletop + 1;
-	i = pilb->piletop + 1;
-	/*while (--i >= 0)
-	{
-		if (pila->list[i] < 19)
-		{
-			hold_first = i;
-			break ;
-		}
-	}
-	i = -1;
-	while (++i >= 0)
-	{
-		if (pila->list[i] < 19)
-		{
-			hold_second = i;
-			break ;
-		}
-	}
-	i = -1;
-	if (pila->piletop - hold_first <= hold_second)
-		while (++i < pila->piletop - hold_first)
-			ft_rotate_pile(pila, pilb, a);
-	else
-		while (++i < hold_second)
-			ft_rotate_reverse_pile(pila, pilb, a);
-	i = -1;
-	ft_push_pile(pila, pilb, a);*/
-	//while (++i < pilb->piletop)
-	//	if
-}
 
 int	ft_is_sorted(t_pile *pila)
 {
@@ -90,7 +52,6 @@ int main(int argc, char **argv)
 		else if (pila->piletop < 5)
 			ft_sort_4_5(pila, pilb);
 	}
-	
 	/*ft_putstr_fd("<---pile A-->\n", 1);
 	ft_printpile(pila);
 	ft_putstr_fd("<---pile B-->\n", 1);
--- srcs/push_swap.h
+++ srcs/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: vchevill <vchevill@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 15:16:13 by vchevill          #+#    #+#             */
-/*   Updated: 2022/01/30 19:15:28 by vchevill         ###   ########.fr       */
+/*   Updated: 2022/01/30 20:06:59 by vchevill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,7 +57,6 @@ void	ft_sort_3_part2(t_pile *pila, t_pile	*pilb);
 void	ft_sort_3(t_pile *pila, t_pile	*pilb);
 void	ft_sort_0_3(t_pile	*pila, t_pile	*pilb);
 void	ft_sort_4_5(t_pile *pila, t_pile *pilb);
-void	ft_sort_4_5_part2(t_pile *pila, t_pile *pilb);
 void	ft_printpile(t_pile *pile);
 int		swap_from_right(int *tab, int i, int j);
 int		swap_from_left(int *tab, int i, int j);
